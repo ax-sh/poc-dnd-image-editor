14ece91e2bd314eecc220b82230ac9e5d88738d6
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-17 15:43:22 +0545

    style: format README and config files consistently

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { PropsWithChildren, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
-
-function App() {
-
+function DropzoneOverlay({ children }: PropsWithChildren) {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <section className={'h-dvh bg-red-500'}>
-ff
-    </section>
-  )
+    <div {...getRootProps()} className={"h-full w-full bg-green"}>
+      <input {...getInputProps()} />
+      {isDragActive
+        ? <p>Drop the files here ...</p>
+        : <p>Drag 'n' drop some files here, or click to select files</p>}
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <section className={"h-dvh bg-red-500"}>
+      <DropzoneOverlay>
+        react-dropzone
+      </DropzoneOverlay>
+    </section>
+  );
+}
+
+export default App;
